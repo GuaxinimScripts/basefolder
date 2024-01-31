1003a752961259e6cc4e00e8422ab83c8a5921f6
janio <sk8.merp@gmail.com>
2024-01-30 21:53:08 -0300

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Guaxinim'
+description 'padrão de pastas'
+version '1.0.0'
+
+client_scripts {
+    'client-side/**',
+}
+server_scripts{
+    'server-side/**'
+}
