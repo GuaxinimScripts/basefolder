f70deaebd0cf7bd2426e0716525f157a73742c9a
janio <sk8.merp@gmail.com>
2024-06-03 12:32:24 -0300

    added shared and UI paths

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,20 @@ author 'Guaxinim'
 description 'padrão de pastas'
 version '1.0.0'
 
+ui_page 'ui/index.html'
+
+shared_scripts{
+    'shared/**'
+}
+
 client_scripts {
     'client-side/**',
 }
 server_scripts{
     'server-side/**'
+}
+
+files {
+    'ui/index.html',
+    'ui/**',
 }
